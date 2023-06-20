9a275de46461266446805a534c429cca7b42de46
Himanshu Chandola <96554303+himanshuchandola@users.noreply.github.com>
2023-06-20 19:37:11 +0530

    changes

---
+++ src/Components/Homepage/HomePage.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import data from '../Data/data.json';
+
+import Paper from '../Paper/Paper'; 
+
+const HomePage = () => {
+  const [city, setCity] = useState('');
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    const optionArr = [];
+    for (let key in data) {
+      optionArr.push({
+        value: data[key].edition_code,
+        label: key
+      });
+    }
+    setOptions(optionArr);
+  }, []);
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  }
+
+  return (
+    <>
+      <div id="app">
+        <div className="paper">
+          <Paper selectedCity={city} />
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default HomePage;
